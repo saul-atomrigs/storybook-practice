ba01b1c4a63b962d0aad4ea30190195fb5bbb5f4
Saul Lee <solleedata@gmail.com>
2023-03-24 13:05:34 +0900

    args 통해 여러 stories 생성

--- pages/component-library/components/Button/Button.stories.tsx
+++ pages/component-library/components/Button/Button.stories.tsx
@@ -8,19 +8,15 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-// story list에 story를 추가:
-export const Primary: ComponentStory<typeof Button> = () => (
-  <Button primary>Button</Button>
-);
+// args를 통해 story를 생성한다. 여기서 Template.bind({})는 함수를 복사하는 자바스크립트 기능이다:
+export const Primary = Template.bind({});
+Primary.args = { backgroundColor: "#ff0", label: "Button" };
 
-// 여러 버전의 story를 추가할 수 있다:
-export const Secondary: ComponentStory<typeof Button> = () => (
-  <Button backgroundColor="#ff0" label="😄👍😍💯" />
-);
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, label: "😄👍😍💯" };
 
-export const Tertiary: ComponentStory<typeof Button> = () => (
-  <Button backgroundColor="#ff0" label="📚📕📈🤓" />
-);
+export const Tertiary = Template.bind({});
+Tertiary.args = { ...Primary.args, label: "📚📕📈🤓" };
 
 // 스토리북 사이드바에 story 이름 변경하기:
 Primary.storyName = "Primary Button";
